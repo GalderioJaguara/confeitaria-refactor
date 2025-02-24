c1d8476dddb3ce4ace56c7f50a0d4a2243b1251d
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-02-24 00:08:10 -0300

    login change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,8 +7,8 @@ import LoginForm from "../components/login/LoginForm";
 export default function Page(){
 
     return (
-       <main className="w-screen h-screen flex justify-center items-center">
-            <div className=" shadow-md border-solid border-primary-500 border-2 md:w-[450px] md:h-[800px] p-8">
+       <main className="md:w-screen min-h-screen flex justify-center items-center">
+            <div className=" overflow-hidden shadow-md border-solid border-primary-500 border-2 md:w-[450px] md:h-[800px] p-8">
                 <h1 className="text-xl font-bold  mb-4">Entre</h1>
                 <p className="text-gray-600">Seja bem vindo(a) ao sistema de gestão Andreia Teófilo Confeitaria</p>
                 <div className="my-8">
