e403515260c4bc027bcea58fcec8e4fc44b8f6d8
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-02-27 22:07:27 -0300

    change color titles

--- src/app/components/dashboard/cards/CardTitle.tsx
+++ src/app/components/dashboard/cards/CardTitle.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 
 export default function CardTitle({children}: {children: ReactNode}) {
     return(
-        <h3 className="font-bold text-2xl">{children}</h3>
+        <h3 className="font-bold text-2xl text-primary-500">{children}</h3>
     );
 }
