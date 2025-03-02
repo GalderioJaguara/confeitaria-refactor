dd0048cb9bbed2f49ca03d39b647b229c17eaaf4
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-03-02 16:32:05 -0300

    log

--- src/app/data/serverActions.ts
+++ src/app/data/serverActions.ts
@@ -3,7 +3,7 @@
 import { revalidateTag } from "next/cache";
 
 export default async function createOrder(formData: FormData) {
-     await fetch("https://confeitaria-refactor.vercel.app/api/orders", {
+    const response =  await fetch("https://confeitaria-refactor.vercel.app/api/orders", {
         method: "POST",
         body: JSON.stringify({
             order_details: formData.get("order_details"),
@@ -13,5 +13,7 @@ export default async function createOrder(formData: FormData) {
             delivery_time: formData.get("delivery_time") 
         }),
     });
+
+    console.log(await response.json());
     revalidateTag("create-order"); 
 }
